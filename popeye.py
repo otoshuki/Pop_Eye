f8c1ff22ce3715b4b8da4eaaba0a4ae05e5527fe
Otoshuki <guiningotoshuki123@gmail.com>
2018-09-24 02:35:49 +0530

    First Commit -added important lines

---
+++ popeye.py
@@ -0,0 +1,46 @@
+"""
+1. Build the Arm Model
+2. Detect Colored Balloons
+3. Detect Balloons in sequence
+4. Algorithm to move arm to required coordinates
+"""
+#Import required libraries
+import cv2
+import numpy as nn
+
+#Detect particular colored balloons
+def detect():
+    #argument - color
+    print('not done yet')
+    #returns coordinates [x,y]
+
+
+#In case balloons of same color
+def dist(x1,y1,x2,y2):
+    #arguments - coordinates x1,y1 and x2,y2
+    dist = nn.sqrt()
+    print('not done yet')
+    #returns distance
+
+
+#Go through balloons in sequence
+def seq():
+    #argument - colors available
+    print('not done yet')
+
+
+
+#Algorithm to move arm to required coordinates
+def move():
+    #arguments - coordinates x,y
+    print('not done yet')
+
+
+#Main function
+def main():
+    print('Not completed yet')
+
+
+#Run the program
+if __name__ == '__main__':
+    main()
